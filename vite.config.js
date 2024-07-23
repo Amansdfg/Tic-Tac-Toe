c5f497194d84a219ea746755036ab90dd49aa8cd
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-24 00:24:51 +0500

    sad

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tic-tac-toe/',  
+  base: '/Tic-Tac-Toe/',  
   plugins: [react()],
   build: {
     outDir: 'dist',
